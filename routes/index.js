4fe3f630a630053b3a66a9b5ad97cd641d150cf8
Brock Altug <brock.altug99@gmail.com>
2025-01-03 01:42:04 -0800

    setup root routing for e-commerce backend: include API routes, default route, and 404 fallback handler

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+
+router.get("/", (req, res) => {
+  res.send("<h1>E-Commerce API Running!</h1>");
+});
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
