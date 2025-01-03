b05a773c62da148c394ddb4d73d874ac5060fc2c
Brock Altug <brock.altug99@gmail.com>
2025-01-03 01:40:04 -0800

    add Sequelize Tag model with columns for id and tag_name, setting up structure for tagging products in e-commerce backend

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
+
+const Product = require("./Product.js"); // Import Product model
+const ProductTag = require("./ProductTag.js"); // Import ProductTag model
 
 class Tag extends Model {}
 
 Tag.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      field: "tag_name",
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
